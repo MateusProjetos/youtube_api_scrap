c101261073885492e54114d95a9b0644a769be75
MateusProjetos <157444313+MateusProjetos@users.noreply.github.com>
2024-07-23 01:39:38 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps : [{
+      name: "api-scraping-videos",
+      script: "server.js",
+      instances: "max",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }]
+  }
